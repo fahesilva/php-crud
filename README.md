a934f77c9d5f31d3047e2e68086abeaaea49824a
Rafael Gomes <fahesilva1993@gmail.com>
2021-01-23 00:21:45 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CRUD com PHP procedural :ne
+
+
+
+- **CREATE** - inserção de dados;
+- **READ** - leitura do que foi escrito.
+- **UPDATE** - atualização de dados.
+- **DELETE** - exclusão de dados. 
+
---
+++ php_action/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+//conexão com o banco 
+$servername = "localhost";
+$username ="root"; 
+$password = "rafael";
+$db_name = "crud";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
+mysqli_set_charset($connect, "utf8");
+
+if(mysqli_connect_error()):
+echo "erro na conexão:" .mysqli_connect_error();
+endif;
